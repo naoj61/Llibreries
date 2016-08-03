4656006f0331dec70e6331fb5b3da691d2b03ba4
joanp <none@none>
2016-08-03 13:17:44 +0200

    Nou control Etiqueta1

---
+++ Controls/Etiqueta1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace Controls
+{
+    partial class Etiqueta1
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(0, 0);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(114, 40);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Etiqueta1";
+            // 
+            // label1
+            // 
+            this.label1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(3, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(108, 19);
+            this.label1.TabIndex = 0;
+            // 
+            // Etiqueta1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Etiqueta1";
+            this.Size = new System.Drawing.Size(114, 40);
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ Controls/Etiqueta1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Controls
+{
+    public partial class Etiqueta1 : UserControl
+    {
+        public Etiqueta1()
+        {
+            InitializeComponent();
+        }
+
+        public string Titol
+        {
+            get { return groupBox1.Text; }
+            set { groupBox1.Text = value; }
+        }
+
+        [Browsable(false)]
+        public override Font Font
+        {
+            get { return base.Font; }
+            set { base.Font = value; }
+        }
+
+        [Browsable(true)]
+        public Font FontTitol
+        {
+            get { return groupBox1.Font; }
+            set { groupBox1.Font = value; }
+        }
+
+        [Browsable(true)]
+        public Font FontText
+        {
+            get { return label1.Font; }
+            set { label1.Font = value; }
+        }
+
+        private decimal vTextN;
+
+        [Browsable(true)]
+        public string TextA
+        {
+            get { return label1.Text; }
+            set
+            {
+                try
+                {
+                    vTextN = Convert.ToDecimal(value);
+                    label1.Text = vTextN.ToString(Mascara);
+                    label1.RightToLeft = RightToLeft.Yes;
+                }
+                catch (FormatException)
+                {
+                    label1.RightToLeft = RightToLeft.No;
+                    label1.Text = value;
+                    vTextN = 0;
+                }
+            }
+        }
+
+
+        private const int Alçada = 40;
+
+
+        public override Size MinimumSize
+        {
+            get { return base.MinimumSize; }
+            set { base.MinimumSize = new Size(value.Width, Alçada); }
+        }
+
+        public override Size MaximumSize
+        {
+            get { return base.MaximumSize; }
+            set { base.MaximumSize = new Size(value.Width, Alçada); }
+        }
+
+
+        [Browsable(true), EditorBrowsable(EditorBrowsableState.Always)]
+        public string Mascara { get; set; }
+
+    }
+}
