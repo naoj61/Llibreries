eb636f4fb5b070fd6c42fd504126c85aaf174c60
joanp <none@none>
2024-02-15 08:13:13 +0100

    Moc "EsTipusNumeric" A Utilitats.

--- Comuns/Controls/DataGridView2.cs
+++ Comuns/Controls/DataGridView2.cs
@@ -6,6 +6,7 @@ using System.Data;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using Comuns;
 
 namespace Controls
 {
@@ -86,36 +87,5 @@ namespace Controls
             }
         }
 
-    }
-
-
-    /// <summary>
-    /// Crea un mètode d'extensió de la classe Type
-    /// </summary>
-    public static class TypeExtensions
-    {
-        public static bool EsTipusNumeric(this Type type)
-        {
-            if (type == null)
-                return false;
-
-            switch (Type.GetTypeCode(type))
-            {
-                case TypeCode.Byte:
-                case TypeCode.SByte:
-                case TypeCode.UInt16:
-                case TypeCode.UInt32:
-                case TypeCode.UInt64:
-                case TypeCode.Int16:
-                case TypeCode.Int32:
-                case TypeCode.Int64:
-                case TypeCode.Decimal:
-                case TypeCode.Double:
-                case TypeCode.Single:
-                    return true;
-                default:
-                    return false;
-            }
-        }
     }
 }
--- Comuns/Utilitats/Utilitats.cs
+++ Comuns/Utilitats/Utilitats.cs
@@ -1166,4 +1166,35 @@ namespace Comuns
 
         #endregion *** Utilitats2 ***
     }
+
+
+    /// <summary>
+    /// Crea un mètode d'extensió de la classe Type
+    /// </summary>
+    public static class TypeExtensions
+    {
+        public static bool EsTipusNumeric(this Type type)
+        {
+            if (type == null)
+                return false;
+
+            switch (Type.GetTypeCode(type))
+            {
+                case TypeCode.Byte:
+                case TypeCode.SByte:
+                case TypeCode.UInt16:
+                case TypeCode.UInt32:
+                case TypeCode.UInt64:
+                case TypeCode.Int16:
+                case TypeCode.Int32:
+                case TypeCode.Int64:
+                case TypeCode.Decimal:
+                case TypeCode.Double:
+                case TypeCode.Single:
+                    return true;
+                default:
+                    return false;
+            }
+        }
+    }
 }
